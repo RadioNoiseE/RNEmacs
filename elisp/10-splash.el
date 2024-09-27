1136cc41805c31b783256d556230928f8cb02bb7
RadioNoiseE <rne@Custom-Party.local>
2024-09-27 19:19:10 +0800

    FIX: splash banner not centered as the frame has not yet been rendered, we refresh it

--- elisp/10-splash.el
+++ elisp/10-splash.el
@@ -1,13 +1,18 @@
 ;; splash screen
 
 (use-package dashboard
-  :init (setq dashboard-startupify-list '(dashboard-insert-banner
-					  dashboard-insert-newline
-					  dashboard-insert-init-info)
-	      dashboard-startup-banner "~/.emacs.d/spl.png"
-	      dashboard-image-banner-max-height 252
-	      dashboard-center-content t
-	      dashboard-vertically-center-content t)
-  :config (dashboard-setup-startup-hook))
+  :init
+  (setq initial-buffer-choice 'dashboard-open)
+  (add-hook 'window-setup-hook (lambda () (dashboard-refresh-buffer)))
+  :config (dashboard-setup-startup-hook)
+  :custom
+  (dashboard-hide-cursor t)
+  (dashboard-buffer-name "*splash*")
+  (dashboard-startupify-list '(dashboard-insert-banner
+			       dashboard-insert-init-info))
+  (dashboard-startup-banner "~/.emacs.d/spl.png")
+  (dashboard-image-banner-max-height 290)
+  (dashboard-center-content t)
+  (dashboard-vertically-center-content t))
 
 (provide '10-splash)
